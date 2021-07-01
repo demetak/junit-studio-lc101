f4ac64fe5fe2bdc794b871b45ea1f684dde48f7f
Demet Akcaoglu <demetakcaoglu@gmail.com>
2021-06-30 19:16:50 -0500

    6.4. Studio: Unit Testing. 12 tests written...

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,8 +10,59 @@ public class BalancedBracketsTest {
     //TODO: add tests here
     @Test
     public void emptyTest() {
-        assertEquals(true, true);
+        assertEquals(BalancedBrackets.hasBalancedBrackets(""), true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void onlyStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("LaunchCode"));
+    }
+
+    @Test
+    public void multipleBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[][]]"));
+    }
+    @Test
+    public void multipleBracketsWithStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[Launch][Code][Rocks!]]"));
+    }
+
+    @Test
+    public void multipleBracketsWithExtraClosingReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[Launch][Code][Rocks!]]]"));
+    }
+
+    @Test
+    public void multipleBracketsWithIncorrectOrderReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]][[[]"));
+    }
+
+    @Test
+    public void multipleBracketsWithIncorrectOrderReturnsFalse2() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launch]][Code[]"));
+    }
+    @Test
+    public void bracketsWithIncorrectOrderReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void bracketsWithIncorrectOrderReturnsFalse2() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+    @Test
+    public void missingBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void missingBracketsReturnsFalse2() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
 
 
 }
